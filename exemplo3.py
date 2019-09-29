c30219bb4875c2649fa72368561efac0274433dd
gabrielreiss <gabrielreissdecastro@gmail.com>
2019-09-28 22:47:30 -0300

    a

--- exemplo3.py
+++ exemplo3.py
@@ -1,12 +1,15 @@
 import numpy as np
+import math
+
 #calcular area do circulo
 
 raio = input("digite o numero do raio do circulo: ") # recebe valor informado pelo usuário
 raio = float(raio) #tranforma a string em float
 
 pi = np.pi #define valor de pi
-area = pi * (raio ** 2) # calcula área do circulo
+pi = math.pi #outra forma de definir o valor de pi
 
+area = pi * (raio ** 2) # calcula área do circulo
 area = round( area, 2 ) #arredonda a área
 
 print("A área é igual a :", area) #exibe a área calculada e arredondada
