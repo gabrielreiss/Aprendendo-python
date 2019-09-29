47d23458b6bf52137c3d21148dd139943ccf5d5b
gabrielreiss <gabrielreissdecastro@gmail.com>
2019-09-28 22:34:44 -0300

    a

--- learn_pandas.py
+++ learn_pandas.py
@@ -1,10 +1,28 @@
 #carregar pacotes
+import numpy as np
 import pandas as pd 
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as skl
 
 #importar dados
 dados = pd.read_csv( "insurance.csv" )
 
+#transformar dados
+dados["region"].unique()
+
+#dados["smoker2"]
+
 #apresentar os primeiros registros
 dados.head()
+
+#verificar NA
+dados.isnull().sum()
+
+#correlações
+dados.corr()
+
+#plot test
+plt.plot(dados["age"],dados["charges"],'o');plt.show()
+
 
